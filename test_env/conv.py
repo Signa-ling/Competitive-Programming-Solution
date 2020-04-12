ec82ad6cd76d2b5ec6243ea598501d24405e0bc8
SPICA\トロ <toro970919@gmail.com>
2020-04-12 23:14:40 +0900

    add 2020/04/12

--- test_env/conv.py
+++ test_env/conv.py
@@ -26,7 +26,7 @@ def main():
     event_name = 'ABC'
 
     # 開催番号
-    event_num = '{}{}'.format(event_name, '155')
+    event_num = '{}{}'.format(event_name, '162')
 
     # 問題番号
     question_num = 'C'
--- test_env/proto.py
+++ test_env/proto.py
@@ -5,15 +5,19 @@ inp = open("./test_env/input.txt", mode="r")
 
 
 # ----ここからプログラム----
-from collections import Counter as cnt
+from math import gcd
+from itertools import combinations as com
 n = int(inp.readline())
-S = [inp.readline().replace('\n', '') for _ in range(n)]
-S = cnt(S)
-m = max(S.values())
-S = sorted(S.items(), key=lambda x: x[0])
-for s, i in S:
-    if i==m: print(s)
+ans = 0
+a = [6*(n-1) for _ in range(1,n)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i%j==0 or j%i==0:
+            print(i, j)
+            ans += min(i, j)
+print(sum(a))
 
+print(ans)
 # ----ここまでプログラム----
 
 
