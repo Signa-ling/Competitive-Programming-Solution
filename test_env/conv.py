52c641e6195df83e31d7f9da1617f9451adde72e
SPICA\トロ <toro970919@gmail.com>
2020-04-21 22:51:15 +0900

    add 2020/04/21

--- test_env/conv.py
+++ test_env/conv.py
@@ -26,7 +26,7 @@ def main():
     event_name = 'ABC'
 
     # 開催番号
-    event_num = '{}{}'.format(event_name, '069')
+    event_num = '{}{}'.format(event_name, '067')
 
     # 問題番号
     question_num = 'C'
--- test_env/proto.py
+++ test_env/proto.py
@@ -8,14 +8,15 @@ inp = open("./test_env/input.txt", mode="r")
 # from collections import Counter
 # import math
 n = int(inp.readline())
-A = list(map(int,inp.readline().split()))
-B = [0]*3
-for i in A:
-    if i%4==0: B[0]+=1
-    elif i%2==0: B[1]+=1
-    else: B[2]+=1
+A = list(map(int, inp.readline().split()))
+ans = 10**9
+x, y = 0, sum(A)
+for i in range(n-1):
+    x += A[i]
+    y -= A[i]
+    ans = min(ans, abs(x-y))
+print(ans)
 
-print('Yes' if B[0]==0 and B[2]==0 or B[0]>=B[2] or B[1]==0 and B[2]-B[0]==1 else 'No')
 # ----ここまでプログラム----
 
 # ファイルのクローズ
